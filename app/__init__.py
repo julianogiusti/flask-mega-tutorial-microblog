31f9e5415292b4fdf6c13c2389724317c19d9a3c
Juliano Giusti <giustijuliano@gmail.com>
2018-10-16 23:11:59 -0300

    Chapter 10, send email, reset password (not working, error says must run connect first...)

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -14,45 +15,48 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-
-from app import routes, models, errors
+mail = Mail(app)
 
 """ PARA TESTAR ENVIO DE EMAIL
-	Para testar envio de email de erro localmente no terminal:
-		export MAIL_SERVER=localhost
-		export MAIL_PORT=8025
-	Para testar envio com server real, usando Gmail:
-		export MAIL_SERVER=smtp.googlemail.com
-		export MAIL_PORT=587
-		export MAIL_USE_TLS=1
-		export MAIL_USERNAME=<your-gmail-username>
-		export MAIL_PASSWORD=<your-gmail-password>
+    Para testar envio de email de erro localmente no terminal:
+    export MAIL_SERVER=localhost
+    export MAIL_PORT=8025
+    Para testar envio com server real, usando Gmail:
+    export MAIL_SERVER=smtp.googlemail.com
+    export MAIL_PORT=587
+    export MAIL_USE_TLS=1
+    export MAIL_USERNAME=<your-gmail-username>
+    export MAIL_PASSWORD=<your-gmail-password>
 """
 if not app.debug:
-	# email
-	if app.config['MAIL_SERVER']:
-		auth = None
-		if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-			auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-		secure = None
-		if app.config['MAIL_USE_TSL']:
-			secure = ()
-		mail_handler = SMTPHandler(
-			mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-			fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-			toaddrs=app.config['ADMINS'],
-			subject='Microblog Failure',
-			credentials=auth, secure=secure)
-		mail_handler.setLevel(logging.ERROR)
-		app.logger.addHandler(mail_handler)
+    # email
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TSL']:
+            secure = ()
+        print(app.config['MAIL_SERVER'])
+        print(auth)
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
-	# log to file
-	if not os.path.exists('logs'):
-		os.mkdir('logs')
-	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
+    # log to file
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
-	app.logger.setLevel(logging.INFO)
-	app.logger.info('Microblog startup')
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors
--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-	# email server config
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-	MAIL_USE_TSL = os.environ.get('MAIL_USE_TSL') is not None
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['giustijuliano@gmail.com']
+    # email server config
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TSL = os.environ.get('MAIL_USE_TSL') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['giustijuliano@gmail.com']
 
-	POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
