e0f2e9c1242c3c2ccfa39ef7dad1047fb4de3aca
Juliano Giusti <giustijuliano@gmail.com>
2018-10-25 21:42:10 -0300

    Chapter 13, Internationalization and Localization (I18n ans L10n)

--- app/__init__.py
+++ app/__init__.py
@@ -1,66 +1,60 @@
-from flask import Flask
-from config import Config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
-import os
+from flask_babel import Babel, lazy_gettext as _l
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
-""" PARA TESTAR ENVIO DE EMAIL
-    Para testar envio de email de erro localmente no terminal:
-    export MAIL_SERVER=localhost
-    export MAIL_PORT=8025
-    Para testar envio com server real, usando Gmail:
-    export MAIL_SERVER=smtp.googlemail.com
-    export MAIL_PORT=587
-    export MAIL_USE_TLS=1
-    export MAIL_USERNAME=<your-gmail-username>
-    export MAIL_PASSWORD=<your-gmail-password>
-"""
 if not app.debug:
-    # email
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
-        if app.config['MAIL_USE_TSL']:
+        if app.config['MAIL_USE_TLS']:
             secure = ()
-        print(app.config['MAIL_SERVER'])
-        print(auth)
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'],
-            subject='Microblog Failure',
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-    # log to file
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 from app import routes, models, errors
